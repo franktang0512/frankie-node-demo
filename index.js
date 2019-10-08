5a76dbbfec01f3ce424708c69c7c6dd603e80b86
Frank Tang <frank051284@gmail.com>
2019-10-09 00:37:23 +0800

    add route and add port

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 //建立Web 服務
 const Express = require('express');
 const app = Express();
+
+//新增路由
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+//監聽
+app.listen(3000, error => {
+    if (error) return console.log(error);
+    console.log('listen http://localhost:3000');
+});
