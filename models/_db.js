f988f3135b4f9a1cc1ca55cdfc0d015c7a85f81c
Frank Tang <frank051284@gmail.com>
2019-10-09 09:55:43 +0800

    build database and export

--- models/_db.js
+++ models/_db.js
@@ -1,2 +1,10 @@
 const Sequelize = require('sequelize');
 const uri = process.env.DATABASE_URL;
+
+const db = new Sequelize(uri, {
+    dialect: 'postgres',
+    dialectOptions: {
+    ssl: true,
+    },
+   });
+   module.exports = db;
