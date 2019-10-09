fd37575065fb3390f527f6911a6eceda9a4afa08
Frank Tang <frank051284@gmail.com>
2019-10-09 10:35:30 +0800

    define User and export

--- models/User.js
+++ models/User.js
@@ -1,2 +1,21 @@
 const Sequelize = require('sequelize');
 const db = require('./_db');
+
+
+const User = db.define('User', {
+    email: {
+    type: Sequelize.STRING(100),
+    unique: true
+    },
+    password: {
+    type: Sequelize.STRING(200)
+    },
+    nickname: {
+    type: Sequelize.STRING
+    },
+    gender: {
+    type: Sequelize.INTEGER
+    }
+   });
+
+   module.exports = User;
